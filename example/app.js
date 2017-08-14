0e09cdc79d50fa7af95e405af5466952363421c5
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-08-14 20:46:27 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,11 +1,15 @@
 var ARKit = require('ti.arkit');
 
+var win = Ti.UI.createWindow({
+  backgroundColor: '#fff'
+});
+
 var sceneView = ARKit.createSceneView({
-	scene: 'Titanium.scnassets/ti-logo.scn' // Create your Scene Assets in Xcode and place them in app/platform/ios (Alloy) or platform/ios (Classic)
+  scene: 'Titanium.scnassets/ti-logo.scn' // Create your Scene Assets in Xcode and place them in app/platform/ios (Alloy) or platform/ios (Classic)
 });
 
 win.addEventListener('open', function() {
-	sceneView.run();
+  sceneView.run();
 });
 
 win.add(sceneView);
